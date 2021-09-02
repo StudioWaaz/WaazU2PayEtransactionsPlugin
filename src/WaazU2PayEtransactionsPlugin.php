abcd063b0d9ed65726cee658947e1686c2e1ee0b
Andde <dev2@studiowaaz.com>
2021-09-02 10:48:19 +0200

    first commit

---
+++ src/WaazU2PayEtransactionsPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Waaz\U2PayEtransactionsPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class WaazU2PayEtransactionsPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
